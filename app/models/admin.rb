ffe443cbacce443828095aa8960e99cc98da1e97
Nathan Brown <nkabrown@gmail.com>
2014-11-03 16:30:35 -0500

    Add Announcement model
    * generate announcement model
    * associate admin and announcement model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,4 +4,6 @@ class Admin < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseInvitable::Inviter
+
+  has_many :announcements
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141103190757) do
+ActiveRecord::Schema.define(version: 20141103212502) do
 
   create_table "admins", force: true do |t|
     t.string   "fname"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 20141103190757) do
   add_index "admins", ["confirmation_token"], name: "index_admins_on_confirmation_token", unique: true
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "announcements", force: true do |t|
+    t.text     "body"
+    t.integer  "admin_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "announcements", ["admin_id"], name: "index_announcements_on_admin_id"
 
   create_table "users", force: true do |t|
     t.string   "fname"
